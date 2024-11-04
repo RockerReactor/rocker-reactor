d9e2fc239114c47ff8c9df426c4e83d59685032e
fmc2003 <72084731+fmc2003@users.noreply.github.com>
2024-11-04 01:05:16 -0500

    beginning of wireframe
    beginning of wireframe

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,55 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <html lang="en">
+      <head>
+        <title>CSS Website Layout</title>
+      </head>
+      <body>
+
+        
+
+        <div className="row">
+          <div className="column control">
+            <div className="row">
+              <div className="column parbox">
+                <div className='parameter'>
+                  <label htmlFor="parIn">Step Size </label>
+                  <input className='parIn' type='text'></input>
+                  <label htmlFor="parIn"> °</label>
+                </div>
+                <div className='parameter'>
+                  <label htmlFor="parIn">Power Min </label>
+                  <input className='parIn' type='text'></input>
+                  <label htmlFor="parIn"> V</label>
+                </div>
+              </div>
+              <div className="column parbox">
+              <div className='parameter'>
+                <label htmlFor="parIn">Cycles </label>
+                <input className='parIn' type='text'></input>
+                <label htmlFor="parIn"> °</label>
+              </div>
+              <div className='parameter'>
+                <label htmlFor="parIn">Power Max </label>
+                <input className='parIn' type='text'></input>
+                <label htmlFor="parIn"> V</label>
+              </div>
+              </div>
+            </div>
+          </div>
+  
+          <div className="column view">
+            <h2>Main Content</h2>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet pretium urna. Vivamus venenatis velit nec neque ultricies, eget elementum magna tristique. Quisque vehicula, risus eget aliquam placerat, purus leo tincidunt eros, eget luctus quam orci in velit. Praesent scelerisque tortor sed accumsan convallis.</p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet pretium urna. Vivamus venenatis velit nec neque ultricies, eget elementum magna tristique. Quisque vehicula, risus eget aliquam placerat, purus leo tincidunt eros, eget luctus quam orci in velit. Praesent scelerisque tortor sed accumsan convallis.</p>
+          </div>
+        </div>
+      </body>
+    </html>
+      
+    
+
   );
 }
 
