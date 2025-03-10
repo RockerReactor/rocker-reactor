ae57180534e12a03f4273f39945fb2d28ca62125
Garrett Engstrom <garrett.engstrom@maine.edu>
2025-03-10 13:09:40 -0400

    Save Button Logic
    Added Logic for our Save Button Component

--- src/components/toolbar/SaveButton.tsx
+++ src/components/toolbar/SaveButton.tsx
@@ -1,10 +1,18 @@
+import { useContext } from "react";
+import AppContext from "../../context/AppContext";
+import "../App.css";
+
+
 const SaveButton = () => {
+
+    const {saveMovementSequence} = useContext(AppContext);
+
     const handleSave = () => {
         console.log("Save functionality to be implemented later.");
     };
 
     return (
-        <button type="button" id="save" className="tbbutton" onClick={handleSave}>
+        <button type="button" id="save" className="tbbutton" onClick={saveMovementSequence}>
             💾
         </button>
     );
