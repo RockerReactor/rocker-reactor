8e06e46289ef0b6c08105720ac4247f16c1bc766
Edaran123 <elijah.daran@gmail.com>
2025-04-01 17:34:16 -0400

    Configured Jest to use AppProvider contexxt

---
+++ src/__tests__/AppTest.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from '../App';
+import { AppProvider } from "../context/AppContext.jsx";
+import { link } from 'fs';
+
+it('renders learn react link', () => {
+
+  render(
+  <AppProvider>
+    <App />
+  </AppProvider>
+  
+
+);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+  
+});
