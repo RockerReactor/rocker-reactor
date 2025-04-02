976c491d96f03bba07b484ba6d2c583b65071d7a
Edaran123 <elijah.daran@gmail.com>
2025-04-01 20:45:53 -0400

    intentionally failing prettier check

--- src/App.tsx
+++ src/App.tsx
@@ -17,8 +17,7 @@ interface Instruction {
 
 function App() {
     const {
-        PWM,
-        setPWM,
+                                       PWM, setPWM,
         SPWM,
         setSPWM,
         AngleStep,
